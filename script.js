39289ddb41d8ce451a090f326d64d9225fec048b
Jaxon755 <jaxon201995@student-schoolsofwestfield.org>
2025-06-03 14:03:46 +0000

    get rounds

--- script.js
+++ script.js
@@ -7,7 +7,21 @@ place.appendChild(button);
 }
 
 function addName(){
-    let myName = prompt("Enter name here");
+    let nameBox = document.getElementById("get-name") 
+    let myName = nameBox.value;
     let place = document.getElementById("name");
     place.innerHTML = "<h3>"+myName+"</h3>";
+    let play = document.createElement("button");
+    play.innerHTML = "PlayRPS"
+    play.addEventListener("click", howManyGames)
+    place.appendChild(play);
+    place.style.borderBottom = "none";
+}
+
+function howManyGames(){
+    let title = document.getElementById("title");
+    title.innerHTML = "RPS";
+    let subtitle = document.getElementById("subtitle");
+    subtitle.innerHTML = "How Many Rounds?";
+
 }
